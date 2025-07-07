91063a5a2ce250516f0a819861184f38f9058c78
Lucas <154390549+LucasVince@users.noreply.github.com>
2025-07-07 08:48:06 -0300

    Add files via upload

---
+++ cipher.py
@@ -0,0 +1,21 @@
+def cipher(message, deslocation):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    ciphered_message = ''
+
+    for iLetter in range(len(message)):
+        char = message[iLetter].lower()
+
+        if char == ' ':
+            ciphered_message += ' '
+            continue
+
+        deslocation_index = alphabet.index(char) + deslocation
+
+        if deslocation_index > 25:
+            deslocation_index -= 25
+        
+        ciphered_message += alphabet[deslocation_index]
+
+    return ciphered_message
+
+print(cipher('hello world', 3))
